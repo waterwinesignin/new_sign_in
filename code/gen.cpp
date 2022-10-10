82cb4c607e94099a16601294c3d67881dea5771f
water-wine <923460842@qq.com>
2022-10-10 21:30:05 +0800

    改bug

--- code/gen.cpp
+++ code/gen.cpp
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <vector>
 #include <set>
-#include <map>
+#include  <map>
 using namespace std;
 
 #define MAXSIZE    1024     //定义数据缓冲区大小
@@ -48,31 +48,31 @@ int main(int argc, char *argv[])
     {
         //有5-8位同学缺席了该学期80%的课
         int num1=rand()%4 + 5;//缺席80%的人数
-        set<int> outabsence80;//缺席该课程80%的学号集合
-        GetRandVec(num1, 90, NULL, outabsence80);
+        set<int> outQueXi80;//缺席该课程80%的学号集合
+        GetRandVec(num1, 90, NULL, outQueXi80);
 
-        map<int, set<int> > absence80Info;
-        for(set<int>::const_iterator it=outabsence80.begin();it!=outabsence80.end();it++)
+        map<int, set<int> > QueXi80Info;
+        for(set<int>::const_iterator it=outQueXi80.begin();it!=outQueXi80.end();it++)
         {
             set<int> tmp;
             GetRandVec(16,20,NULL, tmp);
-            absence80Info[*it]=tmp;
+            QueXi80Info[*it]=tmp;
         }
 
         for(int i=0;i<20;i++)//一学期共20次课
         {
             //此外每次课程均还有0-3位同学由于各种原因缺席
             int num2=rand()%4;
-            set<int> outabsence0_3;//其他原因缺席的
+            set<int> outQueXi0_3;//其他原因缺席的
             if(num2>0)
             {
-                GetRandVec(num2, 90, &outabsence80, outabsence0_3);
+                GetRandVec(num2, 90, &outQueXi80, outQueXi0_3);
             }
 
 
             char line[MAXSIZE]={0};
             sprintf(line, "%d_%d=", ke, i+1);
-            for(map<int, set<int> >::const_iterator it2=absence80Info.begin();it2!=absence80Info.end();it2++)
+            for(map<int, set<int> >::const_iterator it2=QueXi80Info.begin();it2!=QueXi80Info.end();it2++)
             {
                 if(it2->second.find(i+1) != it2->second.end())
                 {
@@ -82,7 +82,7 @@ int main(int argc, char *argv[])
                 }
             }
 
-            for(set<int>::const_iterator it3=outabsence0_3.begin();it3!=outabsence0_3.end();it3++)
+            for(set<int>::const_iterator it3=outQueXi0_3.begin();it3!=outQueXi0_3.end();it3++)
             {
                 char tmp[32]={0};
                 sprintf(tmp, "%d,", *it3);
@@ -94,7 +94,6 @@ int main(int argc, char *argv[])
             fputs(line, pf);
         }
     }
-
 
     fclose(pf);
 
